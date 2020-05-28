0f471fe1923732a94609f01c1053e6e61bb8a0a9
sasikanth <sasikanth.b.009@gmail.com>
2020-05-29 05:08:35 +0530

    ss

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const app = express();
-require('./services/passport')
-require('./routes/authRoutes')(app)
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./services/passport");
+
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to db");
+    }
+  }
+);
 
 const PORT = process.env.PORT || 5000;
-
-
 
 app.get("/", (req, res) => {
   res.send("<a href='/auth/google'><button>sign in</button></a>");
 });
 
-
+require("./routes/authRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`running on ${PORT}`);
---
+++ models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+
+const {Schema} = mongoose
+
+const userschema = new Schema({
+    googleId : String
+})
