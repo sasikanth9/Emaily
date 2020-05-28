20b9a7103ef3f0d734f5e8a3f60df4a3f046ec65
sasikanth <sasikanth.b.009@gmail.com>
2020-05-28 23:51:15 +0530

    gg

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const app = express()
+const app = express();
+require('./services/passport')
+require('./routes/authRoutes')(app)
+
+const PORT = process.env.PORT || 5000;
 
 
-const PORT = process.env.PORT || 5000
 
-app.get('/',(req,res) => {
-    res.send('gg');
-})
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'><button>sign in</button></a>");
+});
 
-app.listen(PORT,() => {
-    console.log(`running on ${PORT}`);
-})
+
+
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
