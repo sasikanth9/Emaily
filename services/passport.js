3027ab580c06aede921d7f5597dece34dfab3e91
sasikanth <sasikanth.b.009@gmail.com>
2020-05-31 07:38:16 +0530

    doubt

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,12 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
+// const User= require('../models/User')
+// async function tst(){
+//   const fuser = await User.findOne({ googleId: '105281569582023161135' });
+//   console.log(fuser);
+// }
+// tst()
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -23,16 +29,13 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then((user) => {
-            done(null, user);
-          });
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      } 
+       const user =  await new User({ googleId: profile.id }).save()
+          done(null, user);
     }
   )
 );
